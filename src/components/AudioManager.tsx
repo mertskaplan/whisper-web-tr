85e34658ba8da9879fc343c6bc8d81621c34dddf
Mert S. Kaplan <mail@mertskaplan.com>
2025-09-20 15:04:04 +0300

    Update AudioManager.tsx

--- src/components/AudioManager.tsx
+++ src/components/AudioManager.tsx
@@ -449,7 +449,7 @@ function SettingsModal(props: {
                     </div>
                     {props.transcriber.multilingual && (
                         <>
-                            <label>Kaynak sesi dilini seç:</label>
+                            <label>Ses dilini seç:</label>
                             <select
                                 className='mt-1 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                                 defaultValue={props.transcriber.language}
