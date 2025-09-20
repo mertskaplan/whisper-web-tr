e5679b5772b2cc4babfc6ded8e3c964c6dd6062c
Mert S. Kaplan <mail@mertskaplan.com>
2025-09-20 12:48:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
             <div className='container flex flex-col justify-center items-center'>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>Whisper Web</h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    Doğrudan tarayıcıda çalışan makine öğrenmesi destekli deşifre uygulaması
+                    makine öğrenmesi destekli deşifre uygulaması
                 </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
