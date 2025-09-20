28b2e229f7a76ba65ecc8f2836c1eeffa4acfb62
Mert S. Kaplan <mail@mertskaplan.com>
2025-09-20 14:13:02 +0300

    Update Constants.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -23,9 +23,7 @@ function mobileTabletCheck() {
 const isMobileOrTablet = mobileTabletCheck();
 export default {
     SAMPLING_RATE: 16000,
-    DEFAULT_AUDIO_URL: `https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/${
-        isMobileOrTablet ? "jfk" : "ted_60_16k"
-    }.wav`,
+    DEFAULT_AUDIO_URL: `https://mertskaplan.com/assets/file/asaf-halet-celebi-kedi.mp3`,
     DEFAULT_MODEL: "Xenova/whisper-base",
     DEFAULT_SUBTASK: "transcribe",
     DEFAULT_LANGUAGE: "turkish",
