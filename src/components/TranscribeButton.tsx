91d481fd163b4323e5e62e0c7ff9b63f55a711e4
Mert S. Kaplan <mail@mertskaplan.com>
2025-09-20 12:38:25 +0300

    Update TranscribeButton.tsx

--- src/components/TranscribeButton.tsx
+++ src/components/TranscribeButton.tsx
@@ -17,11 +17,11 @@ export function TranscribeButton(props: Props): JSX.Element {
             className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center'
         >
             {isModelLoading ? (
-                <Spinner text={"Loading model..."} />
+                <Spinner text={"Model yükleniyor..."} />
             ) : isTranscribing ? (
-                <Spinner text={"Transcribing..."} />
+                <Spinner text={"Deşifre ediliyor..."} />
             ) : (
-                "Transcribe Audio"
+                "Sesi Deşifre Et"
             )}
         </button>
     );
